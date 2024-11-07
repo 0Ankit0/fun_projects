2f078a6bad4d2c72e6639f04c86b16854dabc84e
0ankit0 <ankitpaudel.1373@gmail.com>
2024-11-07 06:30:12 +0545

    Refactor and relocate project, update dependencies
    Refactored `MyInputViewModel.cs` by removing unused usings, properties, and methods, and updating the `Submit` method. Updated `MyInputWindow.g.i.cs` to reformat code and change `ComboBox` fields to `TextBox`.
    
    Relocated project from `I:\vsix\fun_projects\TemplateWizard` to `D:\Projects\fun_projects\TemplateWizard`, updating paths in `files.json`, `TemplateWizard.csproj.nuget.dgspec.json`, and `TemplateWizard.csproj.nuget.g.props`.
    
    Added `WindowsAPICodePack-Shell` dependency and updated `project.assets.json` to include `WindowsAPICodePack-Core` and `WindowsAPICodePack-Shell` packages. Updated package paths to reflect new user directory.

--- TemplateWizard/Model/MyInputViewModel.cs
+++ TemplateWizard/Model/MyInputViewModel.cs
@@ -1,17 +1,10 @@
-using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.WindowsAPICodePack.Dialogs;
-using Microsoft.CodeAnalysis.CSharp;
 
 namespace TemplateWizard.Windows
 {
@@ -21,10 +14,8 @@ namespace TemplateWizard.Windows
         private string _template;
         private string _fileName;
         private string _filePath;
-        private string _selectedModal;
         private string _selectedTemplate;
 
-        public ObservableCollection<string> Modals { get; set; }
         public ObservableCollection<string> Templates { get; set; }
 
         public string Modal
@@ -67,16 +58,6 @@ namespace TemplateWizard.Windows
             }
         }
 
-        public string SelectedModal
-        {
-            get => _selectedModal;
-            set
-            {
-                _selectedModal = value;
-                OnPropertyChanged();
-            }
-        }
-
         public string SelectedTemplate
         {
             get => _selectedTemplate;
@@ -88,86 +69,12 @@ namespace TemplateWizard.Windows
         }
 
         public ICommand SubmitCommand { get; }
-        public ICommand BrowseFolderCommand { get; }
         public ICommand BrowseTemplateFileCommand { get; }
 
         public MyInputViewModel()
         {
             SubmitCommand = new RelayCommand(Submit);
-            BrowseFolderCommand = new RelayCommand(BrowseFolder);
             BrowseTemplateFileCommand = new RelayCommand(BrowseTemplateFile);
-            LoadModalsAsync().ConfigureAwait(false);
-            LoadTemplates();
-        }
-
-        private async Task LoadModalsAsync()
-        {
-            Modals = new ObservableCollection<string>();
-
-            // Define the folders to check
-            var foldersToCheck = new[] { "Model", "Models" };
-
-            // Register MSBuild instance
-            MSBuildLocator.RegisterDefaults();
-
-            // Get the current solution path
-            var solutionPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "YourSolution.sln");
-
-            using (var workspace = MSBuildWorkspace.Create())
-            {
-                var solution = await workspace.OpenSolutionAsync(solutionPath);
-
-                foreach (var project in solution.Projects)
-                {
-                    foreach (var document in project.Documents)
-                    {
-                        var filePath = document.FilePath;
-
-                        // Check if the document's file path contains any of the specified folders
-                        if (foldersToCheck.Any(folder => filePath.Contains(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folder))))
-                        {
-                            var syntaxTree = await document.GetSyntaxTreeAsync();
-                            var semanticModel = await document.GetSemanticModelAsync();
-
-                            var types = (await semanticModel.SyntaxTree.GetRootAsync()).DescendantNodes()
-                                .OfType<Microsoft.CodeAnalysis.CSharp.Syntax.ClassDeclarationSyntax>()
-                                .Select(c => c.Identifier.Text);
-
-                            foreach (var type in types)
-                            {
-                                Modals.Add(type);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        private void LoadTemplates()
-        {
-            Templates = new ObservableCollection<string>();
-
-            string templateDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Template");
-            if (Directory.Exists(templateDirectory))
-            {
-                var templateFiles = Directory.GetFiles(templateDirectory, "*.tt");
-                foreach (var file in templateFiles)
-                {
-                    Templates.Add(file);
-                }
-            }
-        }
-
-        private void BrowseFolder()
-        {
-            var dlg = new CommonOpenFileDialog
-            {
-                IsFolderPicker = true
-            };
-            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                FilePath = dlg.FileName;
-            }
         }
 
         private void BrowseTemplateFile()
@@ -179,13 +86,12 @@ namespace TemplateWizard.Windows
             if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 SelectedTemplate = dlg.FileName;
-                Templates.Add(SelectedTemplate);
             }
         }
 
         private void Submit()
         {
-            string message = $"Modal: {SelectedModal}\nTemplate: {SelectedTemplate}\nFile Name: {FileName}\nFile Path: {FilePath}";
+            string message = $"Modal: {Modal}\nTemplate: {SelectedTemplate}\nFile Name: {FileName}\nFile Path: {FilePath}";
             System.Windows.MessageBox.Show(message, "Input Values", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
